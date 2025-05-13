ea59cbc2ce018145c670e0abf8e4f3825ebd7a26
Pavel <mr.paxapanda@gmail.com>
2025-05-13 23:07:21 +0300

    update api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '127.0.0.1:8080' })
-
-api.interceptors.request.use(cfg => {
-    const tk = localStorage.getItem('tk')
-    if (tk) cfg.headers.Authorization = `Bearer ${tk}`
-    return cfg
+export default axios.create({
+    baseURL: import.meta.env.VITE_API_URL || '/api'
 })
-
-export default api
