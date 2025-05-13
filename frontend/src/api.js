77b27f83f3d9618240748a423c478f6f130004ec
Pavel <mr.paxapanda@gmail.com>
2025-05-13 22:56:51 +0300

    update port

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '77.221.135.36:8080' })
+const api = axios.create({ baseURL: '127.0.0.1:8080' })
 
 api.interceptors.request.use(cfg => {
     const tk = localStorage.getItem('tk')
